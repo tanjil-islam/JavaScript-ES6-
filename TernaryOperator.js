3349eeb5c3611f2d15e4363689f30baab9ba2f15
tanjil-islam <tanjil.islam040820@gmail.com>
2026-05-07 21:05:11 +0600

    TernaryOperator

---
+++ TernaryOperator.js
@@ -0,0 +1,9 @@
+var age = 18
+
+var type = age >= 18 ? 'adult' : 'child'
+console.log(type)
+
+var age2 = 15
+var type2 = age2 >= 18 ? 'adult' : age2 <= 10 ? 'child' : 'teenager'
+
+console.log(type2)
